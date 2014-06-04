67d26c4cd4383a8506f3c7b6e07f3dceaccb3eaf
Yakir Sitbon <kingyes1@gmail.com>
2014-06-04 23:44:00 +0300

    first commit

---
+++ BodyClassesBehavior.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Author:  Yakir Sitbon.
+ * Website: http://www.yakirs.net/
+ */
+
+class BodyClassesBehavior extends CBehavior {
+	
+	private $_classes = array();
+	
+	protected function _cleanClasses() {
+		$this->_classes = array_unique( $this->_classes );
+	}
+
+	public function addBodyClasses( $classes ) {
+		if ( ! empty( $classes ) ) {
+			if ( is_array( $classes ) ) {
+				foreach ( $classes as $class ) {
+					$this->addBodyClasses( $class );
+				}
+			} else {
+				$this->_classes[] = $classes;
+			}
+		}
+	}
+
+	public function getBodyClasses( $classes = array() ) {
+		$this->addBodyClasses( $classes );
+
+		$this->addBodyClasses( 'controller-' . Yii::app()->controller->id );
+		$this->addBodyClasses( 'action-' . Yii::app()->controller->action->id );
+
+		if ( Yii::app()->user->isGuest )
+			$this->addBodyClasses( 'not-logged-in' );
+		else
+			$this->addBodyClasses( 'logged-in' );
+
+		$actionParams = Yii::app()->controller->actionParams;
+		if ( ! empty( $actionParams ) ) {
+			foreach ( $actionParams as $key => $value ) {
+				$this->addBodyClasses( $key . '-' . $value );
+			}
+		}
+
+		$this->_cleanClasses();
+		return CHtml::encode( implode( ' ', $this->_classes ) );
+	}
+	
+}
---
+++ README.md
@@ -0,0 +1,32 @@
+# Yii Body Classes #
+
+This behavior makes adding CSS body classes to your layouts quite simple
+
+## How to use ##
+
+Unpack release in the `protected/extensions/yii-body-classes` folder.
+
+Then add the behavior to a controller (or even better to your components/Controller.php):
+
+```
+public function behaviors() {
+	return array(
+        'BodyClassesBehavior' => array(
+            'class' => 'ext.yii-body-classes.BodyClassesBehavior'
+        ),
+        ...
+    );
+}
+```
+
+In your `layouts/main.php` view, add to your body tag:
+
+```
+</head>
+...
+<body class="<?php echo $this->getBodyClasses(); ?>">
+```
+
+And you're done!
+
+The idea of this code I get from this repo: https://github.com/acorncom/BodyClassBehavior
